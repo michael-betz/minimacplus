6ed2d548e32bcc546e6713455732919da4d7d1b0
Michael Betz <michibetz@gmail.com>
2024-02-05 02:46:30 +0100

    use bash scripts for flashing for now

---
+++ firmware/flashhd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -n "$1" ]; then
+	echo "Usage: $0 mac_plus_rom.bin /dev/ttyUSBx"
+fi
+
+PORT=/dev/ttyUSB0
+FILE=../mame/hd.img
+if [ -n "$1" ]; then PORT=$1; fi
+if [ -n "$2" ]; then FILE=$2; fi
+
+python $HOME/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32 --port $PORT --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x120000 $FILE
+
