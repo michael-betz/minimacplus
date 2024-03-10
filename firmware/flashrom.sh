652bd86ee57da4f7ee481edf5c1a89caf29651d7
Michael Betz <michibetz@gmail.com>
2024-03-10 16:47:09 +0100

    tested on ESP, boots into ROM
    still doesn't find HD

--- firmware/flashrom.sh
+++ firmware/flashrom.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-if [ -n "$1" ]; then
-	echo "Usage: $0 mac_plus_rom.bin /dev/ttyUSBx"
-fi
+echo "Usage: $0 /dev/ttyUSBx mac_plus_rom.bin"
 
-PORT=/dev/ttyUSB0
-FILE=../mame/macplus.rom
-if [ -n "$1" ]; then PORT=$1; fi
-if [ -n "$2" ]; then FILE=$2; fi
-
-python $HOME/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32 --port $PORT --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x100000 $FILE
+python $HOME/.platformio/packages/tool-esptoolpy/esptool.py --chip esp32 --port $1 --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode qio --flash_freq 80m --flash_size detect 0x100000 $2
 
